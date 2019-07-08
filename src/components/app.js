9949f9dde0fa571196c563660349afdf7722196d
Takahashi, Shinya | Shinya | COOMD <shinya.takahashi@rakuten.com>
2019-07-08 20:00:29 +0900

    add styled components

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import style from './style.css';
+import styled, { css } from 'styled-components';
+// import style from './style.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: ${props => (props.subTitle ? 'red' : 'white')};
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: #fff;
+  `}
+`;
+
+const Text = styled(Title)`
+  font-size: 1em;
+`;
+
+const ReverseText = props =>
+<p {...props} children={props.children.split('').reverse()}></p>
 
 const App = () => (
   <>
-    <div className={style.outer}>Test</div>
-    <div className={style.inner}>Test</div>
+    <Title subTitle>ssss</Title>
+    <Text primary as="a" href="#">aiueo</Text>
+    <Text primary as={ReverseText} href="#">aiueo</Text>
+    {/* <div className={style.outer}>Test</div>
+    <div className={style.inner}>Test</div> */}
   </>
 );
 
